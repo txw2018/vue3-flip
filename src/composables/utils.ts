c33bdd1d92a2cae561257a8e100b8927656a4c53
tangxianwen <1658172479@qq.com>
2022-04-19 21:45:04 +0800

    feat: init

---
+++ src/composables/utils.ts
@@ -0,0 +1,37 @@
+export const getRandomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+export const shuffle = (arr: any[]) => {
+  const ret = arr.slice()
+  for (let i = 0; i < ret.length; i++) {
+    const j = getRandomInt(0, i)
+    const t = ret[i]
+    ret[i] = ret[j]
+    ret[j] = t
+  }
+  return ret
+}
+export const preload = (imgs: any[]): Promise<void> => {
+  return new Promise((resolve) => {
+    if (!imgs.length)
+      resolve()
+
+    const length = imgs.length
+    let count = 0
+
+    const load = (src: string) => {
+      const img = new Image()
+      const checkIfFinished = () => {
+        count++
+        if (count === length)
+          resolve()
+      }
+
+      img.onload = checkIfFinished
+      img.onerror = checkIfFinished
+
+      img.src = src
+    }
+    imgs.forEach(load)
+  })
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { images } from '~/composables/mock'
+const imgRefs = ref([])
+
+function shuffle() {
+
+}
+onMounted(() => {
+})
+
+</script>
+
+<template>
+  <button class="m-3 text-sm btn" @click="shuffle">
+    shuffle
+  </button>
+  <div flex flex-wrap pt10>
+    <img v-for="(img,i) in images" :ref="el => { if (el) imgRefs[i] = el }" :key="img" w-30 m10 inline-block :src="img">
+  </div>
+</template>
+†åå
